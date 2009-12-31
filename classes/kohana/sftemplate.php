60f91b24ba2de883ae9651947e8dff4ff8229f4c
Lindsey <lindsey@phparadox.com>
2010-01-01 03:12:58 +1100

    add base sfTemplate wrapper

---
+++ classes/kohana/sftemplate.php
@@ -0,0 +1,41 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Kohana wrapper for the Symfony Components template engine
+ */
+class Kohana_sfTemplate {
+
+	/**
+	 * @var object Kohana_sfTemplate object
+	 */
+	public static $instance;
+
+	/**
+	 * @var array
+	 */
+	public static $config;
+
+	public static function instance() {
+		if (!Kohana_sfTemplate::$instance) {
+			Kohana_sfTemplate::$config = Kohana::config('sftemplate');
+
+			$loader = new sfTemplateLoaderFilesystem(Kohana_sfTemplate::$config->template_path);
+
+			Kohana_sfTemplate::$instance = new sfTemplateEngine($loader);
+
+			// load any helpers that we've defined
+			if (!empty(Kohana_sfTemplate::$config->helpers)) {
+				$helpers = array();
+				foreach (Kohana_sfTemplate::$config->helpers as $helper) {
+					$helpers[] = new $helper;
+				}
+
+				$helper_set = new sfTemplateHelperSet($helpers);
+
+				Kohana_sfTemplate::$instance->setHelperSet($helper_set);
+			}
+		}
+
+		return Kohana_sfTemplate::$instance;
+	}
+}
--- config/sftemplate.php
+++ config/sftemplate.php
@@ -1,5 +1,9 @@
 <?php
 return array(
-	'cache' => APPPATH.'../cache/templates',
-	'templates' => APPPATH.'views',
+	'template_path' => APPPATH.'views/%name%.php', // %name% gets interpolated with the passed template name. Note that it can include directory seperators!
+	'helpers' => array(
+		'sfTemplateHelperAssets',
+		'sfTemplateHelperJavascripts',
+		'sfTemplateHelperStylesheets',
+	),
 );
