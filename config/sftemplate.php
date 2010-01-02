3b2d6a0b9a0da886609efe2249e9a1fa627c2a91
Lindsey <lindsey@phparadox.com>
2010-01-02 14:42:08 +1100

    help text update

--- config/sftemplate.php
+++ config/sftemplate.php
@@ -2,7 +2,7 @@
 return array(
 	'template_path' => APPPATH.'views/%name%.php', // %name% gets interpolated with the passed template name. Note that it can include directory seperators!
 	
-	// override hepers by setting them to false in your application config
+	// Override helpers by setting 'helpers' to null, or by individually setting them to false in your application config
 	'helpers' => array(
 		'sfTemplateHelperAssets' => true,
 		'sfTemplateHelperJavascripts' => true,
